3ad7a8ce9db5698b7212d1590ae0c63f6ee3b085
dtannyc1 <dtannyc1@gmail.com>
2023-06-11 14:56:17 -0400

    add csrf and cors protection

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,12 @@ var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const cors = require('cors');
+const { isProduction } = require('./config/keys');
+const csurf = require('csurf');
+
 var usersRouter = require('./routes/api/users');
+const csrfRouter = require('./routes/api/csrf');
 
 var app = express();
 
@@ -11,6 +16,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+if (!isProduction) {
+    app.use(cors());
+}
+
+app.use(
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true
+        }
+    })
+);
+
 app.use('/api/users', usersRouter);
+app.use('/api/csrf', csrfRouter);
 
 module.exports = app;
---
+++ backend/routes/api/csrf.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const { isProduction } = require('../../config/keys');
+
+if (!isProduction) {
+    router.get("/restore", (req, res) => {
+        const csrfToken = req.csrfToken();
+        res.status(200).json({
+            'CSRF-Token': csrfToken
+        });
+    });
+}
+
+module.exports = router;
