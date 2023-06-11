3b9b1b3846df94537eca938011651c704bb90c37
dtannyc1 <dtannyc1@gmail.com>
2023-06-11 14:24:57 -0400

    set up basic express routing

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,8 @@
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/api/users');
 
 var app = express();
 
@@ -12,9 +10,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/users', usersRouter);
 
 module.exports = app;
