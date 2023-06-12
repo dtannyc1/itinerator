662d3a75ebabbdadf2ea471e3b92810a827cb3c2
dtannyc1 <dtannyc1@gmail.com>
2023-06-12 10:46:01 -0400

    add itinerary index route

--- backend/routes/api/itineraries.js
+++ backend/routes/api/itineraries.js
@@ -1,14 +1,36 @@
 const express = require('express');
-const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
+const User = mongoose.model('User');
 const Itinerary = mongoose.model('Itinerary');
-const passport = require('passport');
 const { isProduction } = require('../../config/keys');
 
 const router = express.Router();
 
-// GET /itineraries/:id, show
-// GET /itineraries, index
-// POST /itineraries, create
-// UPDATE /itineraries, update
-// DELETE /itineraries, delete
+// GET /:id, show
+// GET /, index of most recent itineraries
+
+// GET /users/:id, index of itineraries for specific user
+router.get('/user/:userId', async (req, res, next) => {
+    let user;
+    try {
+        user = await User.findById(req.params.userId);
+    } catch(err) {
+        const error = new Error('User not found');
+        error.statusCode = 404;
+        error.errors = { message: "No user found with that id" };
+        return next(error);
+    }
+    try {
+        const itineraries = await Itinerary.find({ author: user._id })
+                                        .sort({ createdAt: -1 })
+                                        .populate("author", "_id username");
+        return res.json(itineraries);
+    }
+    catch(err) {
+      return res.json([]);
+    }
+})
+
+// POST /, create
+// UPDATE /:id, update
+// DELETE /:id, delete
