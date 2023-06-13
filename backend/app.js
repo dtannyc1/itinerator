88d7f786701a2050f3e86b22688ffb9e892a0e1e
dtannyc1 <dtannyc1@gmail.com>
2023-06-13 14:14:03 -0400

    made changes to work on render

--- backend/app.js
+++ backend/app.js
@@ -25,17 +25,37 @@ app.use(passport.initialize());
 
 if (!isProduction) {
     app.use(cors());
-}
 
-app.use(
-    csurf({
-        cookie: {
-            secure: isProduction,
-            sameSite: isProduction && "Lax",
-            httpOnly: true
-        }
-    })
-);
+    app.use(
+        csurf({
+            cookie: {
+                secure: isProduction,
+                sameSite: isProduction && "Lax",
+                httpOnly: true
+            }
+        })
+    );
+} else {
+    const path = require('path');
+    // Serve the frontend's index.html file at the root route
+    app.get('/', (req, res) => {
+      res.cookie('CSRF-TOKEN', req.csrfToken());
+      res.sendFile(
+        path.resolve(__dirname, '../frontend', 'build', 'index.html')
+      );
+    });
+
+    // Serve the static assets in the frontend's build folder
+    app.use(express.static(path.resolve("../frontend/build")));
+
+    // Serve the frontend's index.html file at all other routes NOT starting with /api
+    app.get(/^(?!\/?api).*/, (req, res) => {
+      res.cookie('CSRF-TOKEN', req.csrfToken());
+      res.sendFile(
+        path.resolve(__dirname, '../frontend', 'build', 'index.html')
+      );
+    });
+}
 
 app.use('/api/users', usersRouter);
 app.use('/api/csrf', csrfRouter);
