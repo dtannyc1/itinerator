f9972f91ec1d873fe81d7e528fca9d3180be7166
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-06-13 14:36:54 -0400

    Add search bar and circle items to get started bar

--- frontend/src/components/MainPage/GetStarted.jsx
+++ frontend/src/components/MainPage/GetStarted.jsx
@@ -3,14 +3,18 @@ import './GetStarted.css'
 const GetStarted = () => {
     return (
         <div className="get-started-wrap">
-            <div className='circle'></div>
-            <div className='circle'></div>
-            <div className='circle'></div>
-            <div className='circle'></div>
-            <div className='circle'></div>
-            <div className='circle'></div>
-            <div className='circle'></div>
-            <div className='circle'></div>
+            <div className='circle'>Museum</div>
+            <div className='circle'>Park</div>
+            <div className='circle'>Bar</div>
+            <div className='circle'>Restaurant</div>
+            <div className='circle'>Soccer</div>
+            <div className='circle'>Basketball</div>
+            <div className='circle'>Bowling</div>
+            <div className='circle'>Pool</div>
+            <input 
+                className='search-bar'
+                placeholder="Location"
+            />
         </div>
     )
 }
