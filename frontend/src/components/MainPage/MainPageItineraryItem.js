eba624e658ef4d736bcf77f3746f6f365c266954
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-06-13 21:37:23 -0400

    Add slider for main page items

--- frontend/src/components/MainPage/MainPageItineraryItem.js
+++ frontend/src/components/MainPage/MainPageItineraryItem.js
@@ -1,12 +1,44 @@
+import Slider from 'react-slick';
 
+export function formatDate(dateString) {
+    const options = { year: 'numeric', month: 'short', day: 'numeric' };
+    const date = new Date(dateString);
+    return date.toLocaleDateString('en-US', options);
+}
 
 const MainPageItineraryItem = ({ itinerary }) => {
-    const { _id, creator } = itinerary;
+    const { _id, creator, createdAt, activities } = itinerary;
+
+    const sliderSettings = {
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        speed: 1000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        pauseOnHover: false,
+        autoplaySpeed: 3000,
+        arrows: false
+    };
+
 
     return (
-        <a href={`/itineraries/${_id}`}>
+        <a className='itinerary-link' href={`/itineraries/${_id}`}>
             <div className="itinerary-item-wrap">
-                <p>{creator}</p>
+                <Slider {...sliderSettings}>
+                    {activities[0].photoURLs.map((photoURL, index) => (
+                        <div key={index}>
+                            <img className="main-activity-img" src={photoURL} alt="activity_photo" />
+                        </div>
+                    ))}
+                </Slider>
+
+                <div className="main-activity-title">Best day ever!</div>
+
+                <div className="main-activity-creator">Shared by:<p>{creator}</p></div>
+
+                <div className="main-activity-date">{formatDate(createdAt)}</div>
             </div>
         </a>
     )
