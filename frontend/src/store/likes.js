83d36bda34e5c50c877c4ea1c62e312ea9586b0c
dtannyc1 <dtannyc1@gmail.com>
2023-06-14 16:44:10 -0400

    add createLike and deleteLike thunk actions, import on line 2 of frontend/store/index.js needs fixing

---
+++ frontend/src/store/likes.js
@@ -0,0 +1,21 @@
+import jwtFetch from "./jwt";
+import receiveItinerary from './itineraries.js';
+
+// thunk action creators
+export const createLike = (itineraryId) => async dispatch => {
+    jwtFetch(`/api/itineraries/${itineraryId}/likes`, {
+        method: 'POST'
+    }).then(res => res.json()).then(data => {
+        dispatch(receiveItinerary(data));
+    })
+}
+
+export const deleteLike = (itineraryId) => async dispatch => {
+    jwtFetch(`/api/itineraries/${itineraryId}/likes`, {
+        method: 'DELETE'
+    }).then(res => res.json()).then(data => {
+        dispatch(receiveItinerary(data));
+    })
+}
+
+// no reducer, itineraries reducer should handle it
