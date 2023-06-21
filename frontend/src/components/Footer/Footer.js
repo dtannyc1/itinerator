1c87ab814e28cdce0fa05be838f4573f66ce11d7
dtannyc1 <dtannyc1@gmail.com>
2023-06-21 14:42:02 -0400

    fixed footer external links

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,20 +1,28 @@
 import './Footer.css';
 
+const openExternalLink = (e, link) => {
+    e.preventDefault();
+    window.open(link, "_blank")
+}
+
 const Footer = () => {
     return (
         <div className="footer-wrap">
             <div className='team-member-capsule'>
                 <div className='footer-name-holder'>David Tan</div>
                 <div className='footer-links-holder'>
-                    <a href='https://www.linkedin.com/in/dtannyc1/'>
+                    <a href='https://www.linkedin.com/in/dtannyc1/'
+                        onClick={e => openExternalLink(e, 'https://www.linkedin.com/in/dtannyc1/')}>
                         <i className="fa-brands fa-linkedin fa-2xl"></i>
                     </a>
 
-                    <a href='https://www.linkedin.com/in/dtannyc1/'>
+                    <a href='https://wellfound.com/u/david-tan-47'
+                        onClick={e => openExternalLink(e, 'https://wellfound.com/u/david-tan-47')}>
                         <i className="fa-brands fa-angellist fa-2xl"></i>
                     </a>
 
-                    <a href='https://github.com/dtannyc1'>
+                    <a href='https://github.com/dtannyc1'
+                        onClick={e => openExternalLink(e, 'https://github.com/dtannyc1')}>
                         <i className="fa-brands fa-github fa-2xl"></i>
                     </a>
                 </div>
@@ -23,15 +31,18 @@ const Footer = () => {
             <div className='team-member-capsule'>
                 <div className='footer-name-holder'>Anton James</div>
                 <div className='footer-links-holder'>
-                    <a href='https://www.linkedin.com/in/anton-james-9b5b18275/'>
+                    <a href='https://www.linkedin.com/in/anton-james-ja/'
+                        onClick={e => openExternalLink(e, 'https://www.linkedin.com/in/anton-james-ja/')}>
                         <i className="fa-brands fa-linkedin fa-2xl"></i>
                     </a>
 
-                    <a href='https://www.linkedin.com/in/anton-james-9b5b18275/'>
+                    <a href='https://wellfound.com/u/anton-james'
+                        onClick={e => openExternalLink(e, 'https://wellfound.com/u/anton-james')}>
                         <i className="fa-brands fa-angellist fa-2xl"></i>
                     </a>
 
-                    <a href='https://github.com/AntonJames-Sistence'>
+                    <a href='https://github.com/AntonJames-Sistence'
+                        onClick={e => openExternalLink(e, 'https://github.com/AntonJames-Sistence')}>
                         <i className="fa-brands fa-github fa-2xl"></i>
                     </a>
                 </div>
@@ -39,15 +50,18 @@ const Footer = () => {
             <div className='team-member-capsule'>
                 <div className='footer-name-holder'>Brandon Choi</div>
                 <div className='footer-links-holder'>
-                    <a href='https://www.linkedin.com/in/brandonchoi28/'>
+                    <a href='https://www.linkedin.com/in/brandonchoi28/'
+                        onClick={e => openExternalLink(e, 'https://www.linkedin.com/in/brandonchoi28/')}>
                         <i className="fa-brands fa-linkedin fa-2xl"></i>
                     </a>
 
-                    <a href='https://www.linkedin.com/in/brandonchoi28/'>
+                    <a href='https://www.linkedin.com/in/brandonchoi28/'
+                        onClick={e => openExternalLink(e, 'https://www.linkedin.com/in/brandonchoi28/')}>
                         <i className="fa-brands fa-angellist fa-2xl"></i>
                     </a>
 
-                    <a href='https://github.com/bchoi28'>
+                    <a href='https://github.com/bchoi28'
+                        onClick={e => openExternalLink(e, 'https://github.com/bchoi28')}>
                         <i className="fa-brands fa-github fa-2xl"></i>
                     </a>
                 </div>
