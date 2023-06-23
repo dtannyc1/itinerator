5ffd64f37884b88efdc6a8b6d7b8bdd16763f09a
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-06-23 11:33:41 -0400

    Fix comment error

--- frontend/src/components/ItineraryShowPage/CommentForm.js
+++ frontend/src/components/ItineraryShowPage/CommentForm.js
@@ -7,11 +7,17 @@ const CommentForm = () => {
     const {itineraryId} = useParams();
     const [commentBody, setCommentBody] = useState('');
     const dispatch = useDispatch();
+    const [error, setError] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        dispatch(createComment(itineraryId, commentBody));
-        setCommentBody('');
+        if(commentBody === '') {
+            setError('Comment can not be empty')
+        } else {
+            dispatch(createComment(itineraryId, commentBody));
+            setCommentBody('');
+            setError('');
+        }
     };
 
     return (
@@ -19,16 +25,19 @@ const CommentForm = () => {
             <form className="comment-capsule-update">
                 <div className="comment-form-title">Have anything to share?</div>
                 <textarea
-                    className="comment-form-body"
+                    className={error ? "comment-form-body-red" : 'comment-form-body'}
                     id="comment-form-body-create"
                     value={commentBody}
                     onChange={e => setCommentBody(e.currentTarget.value)}
                 />
+                <div className="flex-row">
+                {error? <div className="comment-errors">{error}</div> : <div></div>}
                 <button
                     onClick={handleSubmit}
                     className='form-create-button'
                     >Create Comment
                 </button>
+                </div>
             </form>
         </div>
     )
