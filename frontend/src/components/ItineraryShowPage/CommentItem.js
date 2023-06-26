9b9ec2eaebcee480a38de9139d7a04552cfc22c4
dtannyc1 <dtannyc1@gmail.com>
2023-06-26 17:52:16 -0400

    fix comment body height for non authors

--- frontend/src/components/ItineraryShowPage/CommentItem.js
+++ frontend/src/components/ItineraryShowPage/CommentItem.js
@@ -25,13 +25,13 @@ const CommentItem = ({ comment }) => {
 
     const controlButtons = (
         <div className='comment-form-buttons-holder'>
-            <button 
-                className="form-create-button" 
+            <button
+                className="form-create-button"
                 onClick={e=>setShowUpdateFrom(true)}
                 >Update Comment
             </button>
-            <button 
-                className="form-create-button" 
+            <button
+                className="form-create-button"
                 onClick={handleDelete}
                 >Delete Comment
             </button>
@@ -41,8 +41,8 @@ const CommentItem = ({ comment }) => {
     const commentItem = (
         <div className='comment-capsule'>
             <div className='comment-author'>{author}</div>
-            <div className='comment-body'>{body}</div>
-            <div className='comment-date'>{formatDate(createdAt)}</div>
+            <div className={`comment-body${currentUser?._id === authorId ? '' : ' comment-body-full'}`}>{body}</div>
+            <div className={`comment-date${currentUser?._id === authorId ? ' comment-date-short' : ''}`}>{formatDate(createdAt)}</div>
             {currentUser?._id === authorId ? controlButtons : <></>}
         </div>
     );
@@ -50,14 +50,14 @@ const CommentItem = ({ comment }) => {
     const updateCommentForm = (
         <div className='comment-update-box'>
             <div className='comment-author'>{author}</div>
-            
+
             <textarea
                 className='comment-form-body-update'
                 value={commentBody}
                 onChange={e => setCommentBody(e.currentTarget.value)}
             />
-            <button 
-                onClick={handleUpdate} 
+            <button
+                onClick={handleUpdate}
                 id='comment-update-button'
                 className='form-create-button'
                 >Update Comment
