2e00d34c7928195e5835fe106ecdfde0da0f79e1
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-06-27 14:27:27 -0400

    Bug fixes

--- frontend/src/components/UserItineraries/UserItineraries.js
+++ frontend/src/components/UserItineraries/UserItineraries.js
@@ -6,7 +6,6 @@ import { fetchUserItineraries } from "../../store/userItineraries";
 import { getItineraries } from "../../store/itineraries";
 import UserItinerariesItem from "./UserItinerariesItem";
 import { selectCurrentUser } from "../../store/session";
-import jwtFetch from "../../store/jwt";
 
 const UserItineraries = () => {
     const { userId } = useParams();
@@ -14,30 +13,17 @@ const UserItineraries = () => {
     
     const currentUser = useSelector(selectCurrentUser);
 
-    const fetchOwner = async (id) => {
-        const res = await fetch(`/api/users/${id}`);
-        const data = await res.json();
-        return data;
-    };
-
-    const username = (userId) => {
-        const owner = fetchOwner(userId);
-        console.log(owner)
-        return owner
-    }
-    // console.log(fetchOwner(userId).then((user) => user.username ))
-
     useEffect(() => {
         dispatch(fetchUserItineraries(userId));
-    }, [dispatch]);
+    }, [dispatch, userId]);
 
     const userItineraries = useSelector(getItineraries);
 
     const greetings = () => {
-        if (currentUser._id === userId) {
+        if (currentUser?._id === userId) {
             return (<div className="user-itineraries-header">My Itineraries</div>)
         } else {
-            return (<div className="user-itineraries-header">Itineraries</div>)
+            return (<div className="user-itineraries-header">{userItineraries[0].creator}&nbsp;&nbsp;&nbsp;&nbsp;Itineraries</div>)
         }
     }
 
